505091c4a808b049611dfa133423a10259de34f6
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-09-27 23:33:33 +0300

    Task1

---
+++ Tyuiu.GurinchukAV.Sprint1.Task1.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GurinchukAV.Sprint1.Task1.V11.Lib;
+namespace Tyuiu.GurinchukAV.Sprint1.Task1.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 6.0;
+            double y = 1.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(5, res);
+        }
+    }
+}
---
+++ Tyuiu.GurinchukAV.Sprint1.Task1.V11/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.GurinchukAV.Sprint1.Task1.V11.Lib;
+
+namespace Tyuiu.GurinchukAV.Sprint1.Task1.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила : Гуринчук А. В. | ИБКСб-25-1";
+            //Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Гуринчук Анастасия Витальевна | ИБКСб-25-1                   *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет результат по формуле 5*x/(6*y) и печатает его на экране.      *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+            double x, y;
+
+            Console.WriteLine("Введите значение x:");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите значение y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(ds.Calculate(x,y));
+            Console.ReadLine();
+        }
+    }
+}
