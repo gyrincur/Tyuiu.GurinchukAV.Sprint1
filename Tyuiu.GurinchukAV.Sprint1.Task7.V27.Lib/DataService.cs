22f959f8e329c01ccac3ca42940b6120db8ae919
Анастасия Гуринчук <gurincuknasta59@gmail.com>
2025-09-29 15:39:51 +0300

    Task7

--- Tyuiu.GurinchukAV.Sprint1.Task7.V27.Lib/DataService.cs
+++ Tyuiu.GurinchukAV.Sprint1.Task7.V27.Lib/DataService.cs
@@ -5,14 +5,7 @@ namespace Tyuiu.GurinchukAV.Sprint1.Task7.V27.Lib
     {
         public double Calculate(double x, double y)
         {
-            double n1 = Math.Pow(Math.Cos(x), 2) + Math.Pow(Math.Sin(y), 2);
-            double d1 = Math.Sin(y) + 1;
-
-            double n2 = x * y - 12;
-            double d2 = 15 + Math.Cos(x);
-
-            double res = Math.Round(((n1 / d1) - (n2 / d2)), 3);
-            return res;
+            return Math.Round(((Math.Cos(x * x) + Math.Sin(y * y)) / (Math.Sin(y) + 1.0)) - ((x * y) - 12.0) / (15.0 + Math.Cos(x)), 3);
         }
     }
 }
